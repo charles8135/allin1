1b1a66385595bba95400d95cf9a528fc16502745
liuyue <charles8135@gmail.com>
2015-06-03 22:54:56 +0800

    update

--- tools/sh-util/demo.sh
+++ tools/sh-util/demo.sh
@@ -3,7 +3,7 @@
 ### ------ 
 # 使用库函数之前，需要配置一下路径，以便加载各种库函数
 cur_dir=`pwd`
-util_dir=$cur_dir/../inc
+util_dir=$cur_dir/inc
 export PATH=$util_dir:$PATH
 . $util_dir/boot.inc.sh
 ### ------
--- tools/sh-util/phpex-installer.sh
+++ tools/sh-util/phpex-installer.sh
@@ -1,3 +1,31 @@
 #!/bin/bash
 
-. 
+### ------ 
+# 使用库函数之前，需要配置一下路径，以便加载各种库函数
+cur_dir=`pwd`
+util_dir=$cur_dir/inc
+export PATH=$util_dir:$PATH
+. $util_dir/boot.inc.sh
+### ------
+
+ex_dir=$1
+php_dir=$2
+
+if [ ! -d "$ex_dir" ]; then
+    echo_error "$ex_dir does not exist!"
+    exit 1
+fi
+
+if [ ! -d "$php_dir" ]; then
+    echo_error "$php_dir does not exist!"
+    exit 1
+fi
+
+cd $ex_dir
+$php_dir/bin/phpize
+./configure --with-php-config=$php_dir/bin/php-config
+make
+make install
+
+echo_info "$ex_dir install ok"
+exit 0
