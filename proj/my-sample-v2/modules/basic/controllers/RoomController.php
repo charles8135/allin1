7278b1029113748e02274817f96c483b10ddd923
liuyue <charles8135@gmail.com>
2016-08-24 20:13:39 +0800

    update

---
+++ proj/my-sample-v2/modules/basic/controllers/RoomController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace app\modules\basic\controllers;
+
+use app\components\yii\Controller;
+use app\libs\util;
+use app\libs\util\Profiler;
+use app\modules\basic\models;
+use app\modules\basic\logics;
+
+class RoomController extends Controller {
+
+    public function actionCore() {
+        
+    }
+
+}
